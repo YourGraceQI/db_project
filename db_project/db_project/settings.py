943aef98fab1dc016d4059a45a34fc9ab2b9ed28
齐妙 <qimiao@qimiaodeMBP.fios-router.home>
2020-05-07 23:07:40 -0400

    mysql

--- db_project/db_project/settings.py
+++ db_project/db_project/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'db_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'insurance',
+        'USER': 'django',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
